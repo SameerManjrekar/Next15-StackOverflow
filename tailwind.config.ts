7fdcb584f4b96ef9d1c36e087e085d7753c297b0
SameerManjrekar <manjrekarsameer@gmail.com>
2025-01-03 11:38:09 -0600

    Add ShadCn, Theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -10,23 +11,23 @@ export default {
     extend: {
       colors: {
         primary: {
-          100: "#FFF1EC",
-          500: "#FF7000",
+          "100": "#FFF1EC",
+          "500": "#FF7000",
         },
         dark: {
-          100: "#000000",
-          200: "#0F1117",
-          300: "#151821",
-          400: "#212734",
-          500: "#3F4354",
+          "100": "#000000",
+          "200": "#0F1117",
+          "300": "#151821",
+          "400": "#212734",
+          "500": "#3F4354",
         },
         light: {
-          900: "#FFFFFF",
-          800: "#F4F6F8",
-          850: "#FDFDFD",
-          700: "#DCE3F1",
-          500: "#7B8EC8",
-          400: "#858EAD",
+          "400": "#858EAD",
+          "500": "#7B8EC8",
+          "700": "#DCE3F1",
+          "800": "#F4F6F8",
+          "850": "#FDFDFD",
+          "900": "#FFFFFF",
         },
       },
       boxShadow: {
@@ -41,14 +42,15 @@ export default {
         xs: "420px",
       },
       fontFamily: {
-        inter: ["var(--font-inter)", "Inter", "sans-serif"],
-        "space-grotesk": [
-          "var(--font-space-grotesk)",
-          "Space Grotesk",
-          "sans-serif",
-        ],
+        inter: ["var(--font-inter)"],
+        "space-grotesk": ["var(--font-space-grotesk)"],
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
